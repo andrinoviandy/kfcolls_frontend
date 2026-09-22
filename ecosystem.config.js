c7cbb05dfd0e6693954646e6a2b7b5b0c8165ebd
andrinoviandy <andri.noviandy@ilcs.co.id>
2026-09-22 10:27:49 +0700

    update task

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
       args: "/c serve -s build -l 8002",
 
-      cwd: "C:/Programs/kfcolls/frontend",
+      cwd: "C:/Programs/Cost Tracking/kfcolls/frontend",
 
       instances: 1,
 
